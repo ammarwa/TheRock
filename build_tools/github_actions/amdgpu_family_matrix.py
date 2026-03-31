d869d16b5e0b5762af2b5101b89a3547905eb09d
rahulc-gh <rahul.choudhary@amd.com>
2026-03-31 23:20:41 +0000

    adding test config for gfx90a for NPI code

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -241,6 +241,22 @@ amdgpu_family_info_matrix_presubmit = {
             "nightly_check_only_for_family": True,
         },
     },
+    "gfx90a": {
+        "linux": {
+            "test-runs-on": "therock-ci-test-runners",
+            "family": "gfx90a",
+            "fetch-gfx-targets": ["gfx90a"],
+            "sanity_check_only_for_family": True,
+            "build_variants": ["release"],
+        },
+        "windows": {
+            "test-runs-on": "",
+            "family": "gfx90a",
+            "fetch-gfx-targets": [],
+            "build_variants": ["release"],
+            "expect_pytorch_failure": True,
+        },
+    },
 }
 
 # The 'postsubmit' matrix runs on 'push' triggers (for every commit to the default branch).
@@ -311,7 +327,7 @@ amdgpu_family_info_matrix_nightly = {
     },
     "gfx90a": {
         "linux": {
-            "test-runs-on": "linux-gfx90a-gpu-rocm",
+            "test-runs-on": "linux-gfx90a-gpu-npi-rocm",
             "family": "gfx90a",
             "fetch-gfx-targets": ["gfx90a"],
             "build_variants": ["release"],
