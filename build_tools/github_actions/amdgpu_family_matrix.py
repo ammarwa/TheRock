d76cd4e3f3ac44d1ec8ba2b8590ff61eeaf4dd57
rahulc-gh <rahul.choudhary@amd.com>
2026-04-01 23:25:44 +0000

    adding config for gfx1250 test support

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -243,7 +243,7 @@ amdgpu_family_info_matrix_presubmit = {
     },
     "gfx90a": {
         "linux": {
-            "test-runs-on": "therock-ci-test-runners",
+            "test-runs-on": "linux-gfx90a-gpu-npi-rocm",
             "family": "gfx90a",
             "fetch-gfx-targets": ["gfx90a"],
             "sanity_check_only_for_family": True,
@@ -412,6 +412,22 @@ amdgpu_family_info_matrix_nightly = {
             "family": "gfx1153",
             "fetch-gfx-targets": [],
             "build_variants": ["release"],
+        },
+    },
+    "gfx1250": {
+        "linux": {
+            "test-runs-on": "linux-gfx1250-gpu-npi-rocm",
+            "family": "gfx1250",
+            "fetch-gfx-targets": ["gfx1250"],
+            "sanity_check_only_for_family": True,
+            "build_variants": ["release"],
+        },
+        "windows": {
+            "test-runs-on": "",
+            "family": "gfx1250",
+            "fetch-gfx-targets": [],
+            "build_variants": ["release"],
+            "expect_pytorch_failure": True,
         },
     },
 }
